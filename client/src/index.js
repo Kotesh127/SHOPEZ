c1cf0ad481731bd0dd9cf4d8a572afe9015222d3
Kotesh Ambati <136414555+Kotesh127@users.noreply.github.com>
2026-07-14 22:44:03 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import "./App.css";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import "bootstrap-icons/font/bootstrap-icons.css";
+//import "bootstrap-icons/font/bootstrap-icons.css";
 
 import "react-toastify/dist/ReactToastify.css";
 
