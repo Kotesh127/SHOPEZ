614a95dfaa2339596ae3f933924d933040ca3a68
Kotesh Ambati <136414555+Kotesh127@users.noreply.github.com>
2026-07-14 22:38:50 +0530

    Update API base URL to use environment variable

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:
+    process.env.REACT_APP_API_URL ||
+    "https://shopez-rffp.onrender.com/api",
 });
 
 export default API;
